9452f259fcf19292dc36fb8790f1bec14d697d17
serbarbosa <sergiobarbosa@usp.br>
2020-01-26 04:15:08 -0300

    finalizado opinionzoom e enelvo

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ new Vue({
             'API'
             ],
         currSubOp: -1,
+        subOpActive: true,
         subOptions: [
             {
                 ops:[],
@@ -42,13 +43,24 @@ new Vue({
                 ],
                 id: 3
             }
-        ]
-    },
+        ],
+        mainPageSelected: 0,    //variavel para controlar opcoes selecionadas(flexivel)
+    }
+    ,
 
     methods: {
         changeCurrOp(index){
             this.currOption = index
             this.currSubOp = -1
+            this.subOpActive = true;
+
+            //verificar antes se a opcao clicada esta ativa(se estiver, desativar)
+            if(document.getElementsByClassName('option-box')[index].className === 'option-box active'){
+                document.getElementsByClassName('option-box')[index].className = 'option-box'
+                this.subOpActive = false;
+                return
+            }
+
             //controla className para fazer highlight na opcao selecionada
             var prevElem = document.getElementsByClassName('option-box active')
             if(prevElem.length != 0)
@@ -59,13 +71,22 @@ new Vue({
         changeCurrSubOp(subOpIndex){
             this.currSubOp = subOpIndex
 
+            this.mainPageSelected = 0;  //reseta variavel de controle de opcoes
             //controla className para fazer highlight na subopcao selecionada
             var prevElem = document.getElementsByClassName('suboption active')
             if(prevElem.length != 0)
                 prevElem[0].className = 'suboption'
             document.getElementsByClassName('suboption')[subOpIndex].className += ' active'
+
         },
 
+        setSelectionOp(val){
+            this.mainPageSelected = val;
+            var prevOp = document.getElementsByClassName('input-option active')
+            if(prevOp.length != 0)
+                prevOp[0].className = 'input-option'
+            document.getElementsByClassName('input-option')[val].className += ' active'
+        }
     }
 
 })
